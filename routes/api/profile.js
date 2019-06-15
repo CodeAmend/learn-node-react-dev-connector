4502f33a32d7e16e619ebc3a3660e31d9f749d54
Michael Bruce Allen <CodeAmend@Gmail.Com>
2019-06-15 16:29:55 -0400

    Add profile route and create params obj

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator/check');
 const { Users, Profile } = require('../../models');
 const auth = require('../../middleware/auth');
 
@@ -30,6 +31,68 @@ router.get('/me', auth, async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
+
+// @route   POST api/profile 
+// @desc    Create or Update user profile
+// @accress Private
+router.post('/',
+  auth,
+  [
+    check('status', 'status is required').not().isEmpty(),
+    check('skills', 'skills is required').not().isEmpty(),
+  ],
+
+  async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+      console.log("empty");
+      return res.status(400).json({ errors: errors.array() })
+    }
+
+    try {
+
+      const {
+        company,
+        website,
+        location,
+        bio,
+        status,
+        githubusername,
+        skills,
+        youtube,
+        facebook,
+        twitter,
+        instagram,
+        linkedin
+      } = req.body;
+
+
+      const profileFields = {};
+      profileFields.user = req.user.id;
+      if(company) profileFields.company = company;
+      if(website) profileFields.website = website;
+      if(location) profileFields.location = location;
+      if(bio) profileFields.bio = bio;
+      if(status) profileFields.status = status;
+      if(githubusername) profileFields.githubusername = githubusername;
+      if(youtube) profileFields.youtube = youtube;
+      if(facebook) profileFields.facebook = facebook;
+      if(twitter) profileFields.twitter = twitter;
+      if(instagram) profileFields.instagram = instagram;
+      if(linkedin) profileFields.linkedin = linkedin;
+      if(skills) {
+        const skillsArray = skills.split(',');
+        profileFields.skills = skillsArray;
+      }
+
+      res.json(profileFields)
+
+    } catch(err) {
+      res.status(500).json({ msg: 'Server Error'});
+    }
+  }
+);
 
 
 module.exports = router;
