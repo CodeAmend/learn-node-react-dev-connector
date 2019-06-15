aaddf9ca8de24fe7bfa47592b93f136c5a971f42
Michael Bruce Allen <CodeAmend@Gmail.Com>
2019-06-15 15:16:21 -0400

    Add profile/me route and create Profile model

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,35 @@
 const express = require('express');
+const { Users, Profile } = require('../../models');
+const auth = require('../../middleware/auth');
 
 const router = express.Router();
 
-// @route   GET api/profile
-// @desc    Test route
-// @access  Public
-router.get('/', (req, res) => res.send('Profile route'));
+
+// @route   GET api/profile/me
+// @desc    Get current users profile
+// @access  Private
+router.get('/me', auth, async (req, res) => {
+
+  // TODO: What does populate do!! Why does it know which user?
+  try {
+    const profile = await Profile.findOne({ user: req.user.id })
+      .populate('user', ['name', 'avatar']);
+
+
+    if(!profile) {
+      res.status(400).json({
+        msg: "No profile for this user"
+      })
+    }
+
+    res.json(profile);
+    
+
+  } catch(err) {
+    console.log(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 
 module.exports = router;
