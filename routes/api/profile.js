6856d958209663089271cce95970b8e88c2d9a87
Michael Bruce Allen <CodeAmend@Gmail.Com>
2019-06-15 23:36:23 -0400

    Add GET all profiles route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -116,4 +116,19 @@ router.post('/',
 );
 
 
+// @route
+// @desc
+// @access
+router.get('/', (req, res) => {
+  try {
+
+    return res.send('ALL PROFILES');
+
+    
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
